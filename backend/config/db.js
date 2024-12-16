8f7f64380217f48719ba05d33bc38f73b8a515d5
user.email "028swarajkumar@gmail.com <user.email "028swarajkumar@gmail.com>
2024-12-17 02:17:01 +0530

    code added

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,8 +7,6 @@ const connectDB = async () => {
   try {
     // Get MongoDB URI from environment variable
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
