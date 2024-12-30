792bc300efb6cf8087f43add72729a198d1c4dcd
user.email "028swarajkumar@gmail.com <user.email "028swarajkumar@gmail.com>
2024-12-31 04:41:34 +0530

    code changed

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,7 @@ connectDB();
 const app = express();
 
 // Apply CORS middleware
-app.use(cors({
-  origin: 'https://task-1-pm34.onrender.com' // Allow requests from your frontend
-}));// Enable CORS for all origins
+app.use(cors()); // Enable CORS for all origins
 
 app.use(express.json()); // For parsing JSON requests
 app.use("/api/auth", authRoutes); // Authentication routes
