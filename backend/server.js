a22c9593d75819941c55bd1d6f13ecab70bb9712
user.email "028swarajkumar@gmail.com <user.email "028swarajkumar@gmail.com>
2024-12-31 03:53:37 +0530

    code change

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,6 @@ app.use(cors()); // Enable CORS for all origins
 app.use(express.json()); // For parsing JSON requests
 app.use("/api/auth", authRoutes); // Authentication routes
 
-
-// Root route
-app.get("/", (req, res) => {
-  res.send("Backend is running!");
-});
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
