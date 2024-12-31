130dd6e197d2429c3ce5ac4d2ed983c676735fe7
user.email "028swarajkumar@gmail.com <user.email "028swarajkumar@gmail.com>
2024-12-31 06:32:46 +0530

    code changed

--- frontend/src/components/Modals/LoginModal.jsx
+++ frontend/src/components/Modals/LoginModal.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { VITE_BACKEND_URL } from "../constant/backApi"; // Import the backend URL
+import { VITE_BACKEND_URL } from "../constant/backApi";
 
 function LoginModal() {
   const [username, setUsername] = useState("");
