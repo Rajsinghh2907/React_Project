96b14a54b2bbeb197f965a1e204d6fcdaf58f740
user.email "028swarajkumar@gmail.com <user.email "028swarajkumar@gmail.com>
2024-12-31 07:42:21 +0530

    code added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors"); // Import cors
+const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 
@@ -12,18 +12,17 @@ const app = express();
 // Apply CORS middleware
 const corsOptions = {
   origin: ["https://task-1-jade-sigma.vercel.app"], // Allow requests from this origin
-  credentials: true,
+  credentials: true, // Include credentials in the requests
 };
-app.use(cors()); // Enable CORS for all origins// Enable CORS for all origins
-
+app.use(cors(corsOptions)); // Enable CORS with the specified options
 
 app.use(express.json()); // For parsing JSON requests
 app.use("/api/auth", authRoutes); // Authentication routes
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
 app.listen(PORT, () => {
